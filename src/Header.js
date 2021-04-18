7ccb57004f92860f4110b0b9be8cb44ec47a02f8
Swayambhu872 <swayambhunathan@gmail.com>
2021-04-18 19:28:36 +0530

    Styled App.js and Header externally

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import './Header.css'
 
 //functional component
-const headerStyle = {textAlign : 'center', padding : 20, background: '#000', color: '#fff', textTransform: 'uppercase'};
+
 const Header = function() {
     return (
-        <div style = {headerStyle}>
-        Phone Directory
-        
+        <div className = "header">
+        Phone Directory      
     </div>
     )
 }
