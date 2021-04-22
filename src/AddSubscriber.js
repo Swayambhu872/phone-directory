2f2bc1eb1cf3187458de0f4f29d55b0ed0e38ab0
Swayambhu872 <swayambhunathan@gmail.com>
2021-04-22 19:42:09 +0530

    Added routing for Add Subscriber page and Show Subscribers page

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header'
 import './AddSubscriber.css'
+import {Link} from 'react-router-dom'
 
 class AddSubscriber extends Component {
     constructor() {
@@ -10,18 +11,19 @@ class AddSubscriber extends Component {
             name: '',
             phone: ''
         }
-        console.log(this.state);
+       // console.log(this.state);
     }
     inputChangedHandler = (e) => {
         const state = this.state;
         state[e.target.name] = e.target.value;
         this.setState(state);
-        console.log(this.state);
+       // console.log(this.state);
     }
     onFormSubmitted = (e) => {
         e.preventDefault();
         this.props.addSubscriberHandler(this.state);
         this.setState({id: 0, name: '', phone: ''});
+        this.props.history.push("/");
     }
     render() {
         const { name, phone } = this.state;
@@ -29,11 +31,11 @@ class AddSubscriber extends Component {
             <div className="component-container">
                 <Header heading="Add Subscriber" />
                 <div className="component-body-container">
-                    <button className="custom-btn">Back</button>
+                   <Link to="/"> <button className="custom-btn">Back</button></Link>
 
                     <form className="subscriber-form" onSubmit={this.onFormSubmitted.bind(this)}>
                         <label htmlFor="name" className="label-control">Name: </label> <br />
-                        <input id="name" type="text" className="input-control" name="name" onChange={this.inputChangedHandler} /><br /><br />
+                        <input id="name" type="text" className="input-control" name="name"  onChange={this.inputChangedHandler} /><br /><br />
                         <label htmlFor="phone" className="label-control">Phone: </label><br />
                         <input id="phone" type="text" className="input-control" name="phone" onChange={this.inputChangedHandler} /><br /><br />
                         <div className="subscriber-info-container">
